25ca8ef3bde7460a28f128a9d9d858655bbac886
De General <aubynsamuel05@gmail.com>
2025-05-10 13:42:55 +0000

    AI play fixes and recommendations for  smarter AI decisions

--- Assets/Scripts/ChooseCardAI.cs
+++ Assets/Scripts/ChooseCardAI.cs
@@ -13,6 +13,9 @@ public static class CardAI
     public static Card ChooseCardAI(List<Card> hand, Card leadCard, int remainingRounds)
     {
         // If AI is leading/is in control (no lead card)
+        /*   TODO: When we play a card and human doesn't have that suit, keeping it in hand is useless as we 
+          we cant take control with it later so finish up that suit to make sure human plays other cards
+          and waste them to minimize their chance of gaining control */
         if (leadCard == null)
         {
             // Debug.Log("[CardAI] AI is leading");
@@ -23,6 +26,7 @@ public static class CardAI
             }
             else
             {
+                /*  TODO: Fix try to keep control as much as possible if we can */
                 // Otherwise play lowest card to preserve high cards
                 return hand.OrderBy(card => card.value).First();
             }
@@ -43,20 +47,13 @@ public static class CardAI
 
                 if (winningCards.Count > 0)
                 {
-                    // Not sound will be removed in the future to make AI better
-                    if (remainingRounds <= 2)
-                    {
-                        // Play highest winner in final rounds
-                        return winningCards.OrderByDescending(card => card.value).First();
-                    }
-                    else
-                    {
-                        // Play lowest winner in early rounds
-                        return winningCards.OrderBy(card => card.value).First();
-                    }
+                    // Win with the lowest winning card always
+                    return winningCards.OrderBy(card => card.value).First();
                 }
                 else
                 {
+                    /*  TODO: Fix if we have a 6 or 7 of a strong suit in hand, let say a 6, J and K of heart, play J as 
+                    we can take control with K and win with 6 or 7 for 3 or 2 points */
                     // Can't win, so play lowest card of required suit
                     return cardsOfSuit.OrderBy(card => card.value).First();
                 }
